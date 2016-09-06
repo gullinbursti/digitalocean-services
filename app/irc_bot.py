c89b427b626a8493b91313aa03f746ed05af305a
Matt <matt@modd.online>
2016-09-06 16:29:58 -0700

    Making message post dynamic

--- app/irc_bot.py
+++ app/irc_bot.py
@@ -1,4 +1,3 @@
-
 import os, sys
 import socket, requests
 import time, string, json
@@ -7,6 +6,8 @@ import time, string, json
 IRC_SERVER = 'irc.chat.twitch.tv'
 IRC_PORT = 6667
 BUFFER_SIZE = 4096
+
+IRC_MESSAGE = "You have been requested for {game_name} help inside GameBots"
 
 
 def connect_server():
@@ -84,7 +85,7 @@ while "PART" not in messages:
 
   #-- deliver message after getting names list
   if len(messages) >= 2 and messages[-2] == "/NAMES":
-    send_message(sys.argv[3], "I recommend you to help me with {game_name} inside of GameBots! For details please Kik: GameBots.Player".format(game_name=sys.argv[4]))
+    send_message(sys.argv[3], IRC_MESSAGE.format(game_name=sys.argv[4].lower()))
     time.sleep(1)
     timestamp = int(time.time())
     send_command("PING %d" % (timestamp))
