0e3b3992bec279c72f7b86f188bf063615d3e894
Matt Holcombe <matt@modd.live>
2017-01-11 19:41:22 -0800

    broadcaster updates

--- cron/php/fb-product-broadcast.php
+++ cron/php/fb-product-broadcast.php
@@ -6,8 +6,11 @@ define('DB_USER', "db4086_modd_usr");
 define('DB_PASS', "f4zeHUga.age");
 
 define('BROADCASTER_ADDR', "162.243.150.21");
+define('BROADCASTER_USER_AGENT', "GameBots-Broadcaster-v3");
+define('FB_ORTHODOX_MESSAGE_TEMPLATE', "_{PRODUCT_NAME}_ went on sale for \$_{PRICE}_.");
+define('FB_MESSAGE_TEMPLATE_PATH', "/opt/cron/etc/fb-product-broadcast.conf");
 define('FB_GRAPH_API', "https://graph.facebook.com/v2.6/me/messages?access_token=EAAXFDiMELKsBAM0ukSiFZBhCHFWJIqMHhv1uwuL0GZB59PZC7AljrESQetUJRlusUTkzyMnM67Ahn9etkboS4ZCXIRoipiIUIYUh11nx3FQqDRKLxdGZCWSsONZBwQEpjV67GV7majCwB5iTUaaDPoQZC3FAIAxZCeQ5cdqhE9DSMBKS9Gzpv9Yt");
-
+define('STRIPE_URL_TEMPLATE', 'http://prekey.co/stripe/_{PRODUCT_ID}_/_{TO_USER}_');
 
 // make the connection
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Could not connect to database\n");
@@ -29,52 +32,58 @@ if (mysqli_num_rows($result) == 0) {
 $product_obj = mysqli_fetch_object($result);
 mysqli_free_result($result);
 
-
 $date1 = new DateTime($product_obj->added);
 $date2 = new DateTime("now");
 $interval = $date1->diff($date2);
 
-$query = 'SELECT DISTINCT `chat_id` FROM `fbbot_logs`;';
-//$query = 'SELECT DISTINCT `chat_id` FROM `fbbot_logs` WHERE `chat_id` = "1219553058088713" LIMIT 1;';
+// open txt 1-liner for template
+$message_template = FB_ORTHODOX_MESSAGE_TEMPLATE;
+$handle = @fopen(FB_MESSAGE_TEMPLATE_PATH, 'r');
+if ($handle) {
+  
+  // read in the line
+  while (($buffer = fgets($handle, 4096)) !== false) {
+    if (preg_match('/^#(.*)/i', $buffer)) {
+      continue;
+    
+    } else {
+      $message_template = $buffer;
+    }
+  }
+
+  // eof error
+  if (!feof($handle)) {
+    $message_template = FB_ORTHODOX_MESSAGE_TEMPLATE;
+  }
+
+  fclose($handle);
+
+} else {
+  echo("Couldn't open template file, using default: ". FB_ORTHODOX_MESSAGE_TEMPLATE);
+}
+
+
+// replace template tokens w/ content
+$body_txt = preg_replace('/_\{PRODUCT_NAME\}_/', $product_obj->name, $message_template);
+$body_txt = preg_replace('/_\{PRICE\}_/', $product_obj->price, $body_txt);
+$body_txt = preg_replace('/_\{HOURS\}_/', $interval->h, $body_txt);
+$body_txt = preg_replace('/_\{MINUTES\}_/', $interval->m, $body_txt);
+$body_txt = preg_replace('/_\{SECONDS\}_/', $interval->s, $body_txt);
+$stripe_url = preg_replace('/_\{PRODUCT_ID\}_/', $product_obj->id, STRIPE_URL_TEMPLATE);
+
+
+$query = (count($argv) == 2) ? 'SELECT DISTINCT `chat_id` FROM `fbbot_logs` WHERE `chat_id` = "'. $argv[1] .'" LIMIT 1;' : 'SELECT DISTINCT `chat_id` FROM `fbbot_logs`;';
 $result = mysqli_query($db_conn, $query);
 
+echo("Sending to ". number_format(mysqli_num_rows($result)) ." total users w/ message: \"". $body_txt ."\" & card image: [". $product_obj->image_url ."]\n");
+
+$cnt = 0;
 while ($user_obj = mysqli_fetch_object($result)) {
-  echo ("FB --> ". $user_obj->chat_id ."\n");
+  
+  // output status
+  echo(sprintf("FB (%05s/%05s)--> [%-16s]\n", number_format(++$cnt), number_format(mysqli_num_rows($result)), $user_obj->chat_id));
+  
   $payload_arr = array(
-		'recipient' => array(
-			'id' => $user_obj->chat_id
-		),
-		'message'   => array(
-			'attachment' => array(
-				'type'    => "image",
-				'payload' => array(
-					'url' => $product_obj->video_url
-				)
-			),
-			'quick_replies' => array(
-				array(
-					'content_type' => "text",
-					'title'        => "Main Menu",
-					'payload'      => "MAIN_MENU"
-				)
-			)
-		)
-	);
-
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, FB_GRAPH_API);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload_arr));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-
-	$res = json_decode(curl_exec($ch), true);
-	curl_close($ch);
-
-	$payload_arr = array(
 		'recipient' => array(
 			'id'      => $user_obj->chat_id
 		),
@@ -85,21 +94,28 @@ while ($user_obj = mysqli_fetch_object($result)) {
 			    'template_type' => "generic",
 		      'elements'      => array(
 			      array(
-              'title'      => $product_obj->name ." went on sale for $". $product_obj->price,
+              'title'      => $body_txt,
 			         'subtitle'  => "",
-			         'image_url' => "",
-			         'item_url'  => "http://prekey.co/stripe/". $product_obj->id ."/". $user_obj->chat_id,
+			         'image_url' => $product_obj->image_url,
+			         'item_url'  => preg_replace('/_\{TO_USER\}_/', $user_obj->chat_id, $stripe_url),
 			         'buttons'   => array(
-				        array(
-					        'type'  => "web_url",
-                  'url'   => "http://prekey.co/stripe/". $product_obj->id ."/". $user_obj->chat_id,
-                  'title' => "Buy"
-				        )
-			        )
+ 				        array(
+ 					        'type'   => "web_url",
+                   'url'   => preg_replace('/_\{TO_USER\}_/', $user_obj->chat_id, $stripe_url),
+                   'title' => "Buy"
+ 				        )
+               )
 			      )
 		      )
 		    )
-		  )
+		  ),
+		  'quick_replies' => array(
+        array(
+          'content_type' => "text",
+          'title' => "Main Menu",
+          'payload' => "MAIN_MENU"
+        )
+      )
 	  )
 	);
 
@@ -113,15 +129,19 @@ while ($user_obj = mysqli_fetch_object($result)) {
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
 	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 
-	$res = json_decode(curl_exec($ch), true);
+	$res = curl_exec($ch);
 	curl_close($ch);
+	
+	if (strlen($res) > 0) {
+	  echo ("RESULT: ". $res ."\n");  
+	}
 	
 	
   $ch = curl_init();
   curl_setopt_array($ch, array(
-  	CURLOPT_USERAGENT => "GameBots-Tracker-v2",
+  	CURLOPT_USERAGENT      => BROADCASTER_USER_AGENT,
   	CURLOPT_RETURNTRANSFER => true,
-  	CURLOPT_URL => "http://beta.modd.live/api/bot_tracker.php?src=facebook&category=broadcast&action=broadcast&label=". $user_obj->chat_id ."&value=&cid=". md5(BROADCASTER_ADDR)
+  	CURLOPT_URL            => "http://beta.modd.live/api/bot_tracker.php?src=facebook&category=broadcast&action=broadcast&label=". $user_obj->chat_id ."&value=&cid=". md5(BROADCASTER_ADDR)
   ));
 
   $res = curl_exec($ch);
@@ -129,9 +149,9 @@ while ($user_obj = mysqli_fetch_object($result)) {
   
   $ch = curl_init();
   curl_setopt_array($ch, array(
-  	CURLOPT_USERAGENT => "GameBots-Tracker-v2",
+  	CURLOPT_USERAGENT      => BROADCASTER_USER_AGENT,
   	CURLOPT_RETURNTRANSFER => true,
-  	CURLOPT_URL => "http://beta.modd.live/api/bot_tracker.php?src=facebook&category=user-message&action=user-message&label=". $user_obj->chat_id ."&value=&cid=". md5(BROADCASTER_ADDR)
+  	CURLOPT_URL            => "http://beta.modd.live/api/bot_tracker.php?src=facebook&category=user-message&action=user-message&label=". $user_obj->chat_id ."&value=&cid=". md5(BROADCASTER_ADDR)
   ));
 
   $res = curl_exec($ch);
--- cron/php/kik-product-broadcast.php
+++ cron/php/kik-product-broadcast.php
@@ -8,13 +8,14 @@ define('DB_PASS', "f4zeHUga.age");
 
 // bot info
 define('BROADCASTER_ADDR', "162.243.150.21");
+define('BROADCASTER_USER_AGENT', "GameBots-Broadcaster-v3");
 define('KIKBOT_HOST', "http://159.203.250.4:8080");
 define('KIKBOT_WEBHOOK', "/product-notify");
-define('KIKBOT_USER_AGENT', "GameBots-Broadcaster-v3");
 define('KIKBOT_BROADCAST_TOKEN', "326d665bbc91c22b4f4c18a64e577183");
+define('KIKBOT_MESSAGE_TEMPLATE_PATH', "/opt/cron/etc/kik-product_broadcast.conf");
 define('KIKBOT_ORTHODOX_MESSAGE_TEMPLATE', '_{PRODUCT_NAME}_ went on pre-sale _{HOURS}_h _{MINUTES}_m _{SECONDS}_s ago for $_{PRICE}_.');
-define('KIKBOT_PRODUCT_ATTRIBUTION', "Tap to Reserve");
-define('KIKBOT_STRIPE_TEMPLATE', 'http://prekey.co/stripe/_{PRODUCT_ID}_/_{TO_USER}_');
+define('KIKBOT_PRODUCT_ATTRIBUTION', "game.bots");
+define('STRIPE_URL_TEMPLATE', 'http://prekey.co/stripe/_{PRODUCT_ID}_/_{TO_USER}_');
 
 
 // make the connection
@@ -45,12 +46,17 @@ $interval = $date1->diff($date2);
 
 // open txt 1-liner for template
 $message_template = KIKBOT_ORTHODOX_MESSAGE_TEMPLATE;
-$handle = @fopen("/opt/modd/txt/kik-product-broadcast.txt", 'r');
+$handle = @fopen(KIKBOT_MESSAGE_TEMPLATE_PATH, 'r');
 if ($handle) {
   
   // read in the line
   while (($buffer = fgets($handle, 4096)) !== false) {
-    $message .= $buffer;
+    if (preg_match('/^#(.*)/i', $buffer)) {
+      continue;
+    
+    } else {
+      $message_template = $buffer;
+    }
   }
 
   // eof error
@@ -59,11 +65,11 @@ if ($handle) {
   }
 
   fclose($handle);
+  
+} else {
+  echo("Couldn't open template file, using default: ". KIKBOT_ORTHODOX_MESSAGE_TEMPLATE);
 }
 
-// use defined name as argv 'php kik-product-broadcast.php s "btzDoh"'
-$query = (count($argv) == 3 && $argv[1] == "s") ? 'SELECT `username`, `chat_id` FROM `kikbot_logs` WHERE `username` = "'. $argv[2] .'" ORDER BY `added` DESC LIMIT 1;' : 'SELECT DISTINCT(`username`), `chat_id` FROM `kikbot_logs`;';
-$result = mysqli_query($db_conn, $query);
 
 // replace template tokens w/ content
 $body_txt = preg_replace('/_\{PRODUCT_NAME\}_/', $product_obj->name, $message_template);
@@ -71,9 +77,13 @@ $body_txt = preg_replace('/_\{PRICE\}_/', $product_obj->price, $body_txt);
 $body_txt = preg_replace('/_\{HOURS\}_/', $interval->h, $body_txt);
 $body_txt = preg_replace('/_\{MINUTES\}_/', $interval->m, $body_txt);
 $body_txt = preg_replace('/_\{SECONDS\}_/', $interval->s, $body_txt);
-$stripe_url = preg_replace('/_\{PRODUCT_ID\}_/', $product_obj->id, KIKBOT_STRIPE_TEMPLATE);
+$stripe_url = preg_replace('/_\{PRODUCT_ID\}_/', $product_obj->id, STRIPE_URL_TEMPLATE);
 
-echo("Sending to ". number_format(mysqli_num_rows($result)) ." total users w/ message: \"". $body_txt ."\" & card image: [". $product_obj->image_url ."]\n");
+// use defined name as argv 'php kik-product-broadcast.php "btzDoh"'
+$query = (count($argv) == 2) ? 'SELECT `username`, `chat_id` FROM `kikbot_logs` WHERE `username` = "'. $argv[1] .'" LIMIT 1;' : 'SELECT DISTINCT(`username`), `chat_id` FROM `kikbot_logs` WHERE `id` > 105499;';
+$result = mysqli_query($db_conn, $query);
+
+echo("Sending to ". number_format(mysqli_num_rows($result)) ." total users w/ message: \"". $body_txt ."\" & card image: [". $product_obj->image_url ."]\n[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]\n");
 
 // iterate
 $cnt = 0;
@@ -94,7 +104,7 @@ while ($user_obj = mysqli_fetch_object($result)) {
       'chat_id'    => $user_obj->chat_id,
       'item_id'    => $product_obj->id,
       'img_url'    => $product_obj->image_url,
-      'item_url'   => preg_replace('/_\{TO_USER\}_/', $user_obj->username, $stripe_url),
+      'item_url'   => "http://kik.me/game.bots",//preg_replace('/_\{TO_USER\}_/', $user_obj->username, $stripe_url),
       'body_txt'   => $body_txt,
       'attrib_txt' => KIKBOT_PRODUCT_ATTRIBUTION
     )),
