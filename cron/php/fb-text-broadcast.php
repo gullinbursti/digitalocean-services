1b77162e6ce4d8ed73250521b196b57679f04e30
Matt Holcombe <matt@modd.live>
2017-01-21 14:52:57 -0800

    text broadcasting

--- cron/php/fb-text-broadcast.php
+++ cron/php/fb-text-broadcast.php
@@ -5,6 +5,63 @@ define('DB_NAME', "db4086_modd");
 define('DB_USER', "db4086_modd_usr");
 define('DB_PASS', "f4zeHUga.age");
 
+define('BROADCASTER_ADDR', "162.243.150.21");
+define('BROADCASTER_USER_AGENT', "GameBots-Broadcaster-v3");
+define('FB_MESSAGE_TEMPLATE_PATH', "/opt/cron/etc/fb-text-broadcast.conf");
+
+
+function send_tracker($fb_psid) {
+  $tracking_arr = array(
+    array(
+      'category' => "broadcast",
+      'action'   => "broadcast",
+      'label'    => $fb_psid,
+      'value'    => 0,
+      'cid'      => md5(BROADCASTER_ADDR)
+    ),
+    array(
+      'category' => "user-message",
+      'action'   => "user-message",
+      'label'    => $fb_psid,
+      'value'    => 0,
+      'cid'      => md5(BROADCASTER_ADDR)
+    )
+  );
+  
+  $response_arr = array();
+  foreach ($tracking_arr as $val) {
+    $ch = curl_init();
+    curl_setopt_array($ch, array(
+    	CURLOPT_USERAGENT      => BROADCASTER_USER_AGENT,
+    	CURLOPT_RETURNTRANSFER => true,
+    	CURLOPT_URL            => sprintf("http://beta.modd.live/api/bot_tracker.php?src=facebook&category=%s&action=%s&label=%s&value=%d&cid=%s", $val['category'], $val['action'], $val['label'], $val['value'], $val['cid'])
+    ));
+    
+    array_push($response_arr, curl_exec($ch));
+    curl_close($ch);
+  }
+}
+
+function post_message($url, $payload_obj) {
+  $ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload_obj));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+
+	$response = curl_exec($ch);
+	curl_close($ch);
+	
+	if (strlen($response) > 0) {
+	  echo($response ."\n");
+	}
+}
+
+
 // make the connection
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Could not connect to database\n");
 
@@ -12,54 +69,75 @@ $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Could not connect t
 mysqli_select_db($db_conn, DB_NAME) or die("Could not select database\n");
 mysqli_set_charset($db_conn, 'utf8');
 
-// text content file
-$message_body = "";
-$handle = @fopen("/opt/modd/txt/fb-text-broadcast.txt", 'r');
+// open txt config for templates
+$config_arr = array(
+  'FB_GRAPH_API'        => "https://graph.facebook.com/v2.6/me/messages",
+  'FB_ACCESS_TOKEN'     => "EAAXFDiMELKsBADVw92wLSx3GMEpeYcMqgCoFsyw4oZCw2LyMO4MIDJljsVvh4ZAsBp5A9476i7knpaJZAiPpmVnFrRKkJ7DCdWamXJeF0HRKYDMNbJYImDoOmD3B0WmIZBEZAl3jaWusenO6jmUBg1NOEHdGp7ZAV09JxsBUBpVQZDZD",
+  'BODY_TEMPLATE'       => "Want a chance to win a Frontside Misty? Get Gamebots on Kik.me/game.bots"
+);
+
+$handle = @fopen(FB_MESSAGE_TEMPLATE_PATH, 'r');
 if ($handle) {
-  while (($buffer = fgets($handle, 4096)) !== false) {
-    $message_body .= $buffer;
-  }
   
+  // read in the line
+  while (($buffer = fgets($handle, 4096)) !== false) {
+    if (preg_match('/^[#$]/i', $buffer) || preg_match('/^\s*$/i', $buffer)) {
+      continue;
+    
+    } else {
+      preg_match('/^(?P<key>[A-Z_]+)\t(?P<val>.*)$/', $buffer, $match_arr);
+      $config_arr[$match_arr['key']] = $match_arr['val'];
+    }
+  }
+
+  // eof error
   if (!feof($handle)) {
-    $message_body = "__(FGETS-EFFED-UP)__";
+    //$message_template = FB_ORTHODOX_MESSAGE_TEMPLATE;
   }
   
   fclose($handle);
+
+} else {
+  echo("Couldn't open config file! ". FB_MESSAGE_TEMPLATE_PATH ."\n");
 }
 
+$body_txt = $config_arr['BODY_TEMPLATE'];
 
-// matty's fb id
-//$user_obj['chat_id'] = "1219553058088713";
-//$query = 'SELECT DISTINCT `chat_id` FROM `fbbot_logs` WHERE `chat_id` = "1219553058088713" LIMIT 1;';
 
-$query = 'SELECT DISTINCT `chat_id` FROM `fbbot_logs`;';
+// get list of recipients
+$query = (count($argv) == 2) ? 'SELECT DISTINCT `chat_id` FROM `fbbot_logs` WHERE `chat_id` = "'. $argv[1] .'" LIMIT 1;' : 'SELECT DISTINCT `chat_id` FROM `fbbot_logs`;';
 $result = mysqli_query($db_conn, $query);
 
+// summary
+echo("Sending to ". number_format(mysqli_num_rows($result)) ." total users w/ message: \"". $body_txt ."\"\n[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]\n");
+
+// iterate
+$cnt = 0;
 while ($user_obj = mysqli_fetch_object($result)) {
-  echo ("FB --> ". $user_obj->chat_id ."\n");
+  
+  // output status
+  echo(sprintf("FB (%05s/%05s)--> [%-16s]\n", number_format(++$cnt), number_format(mysqli_num_rows($result)), $user_obj->chat_id));
+  
+  // build json array
   $payload_arr = array(
 		'recipient' => array(
 			'id' => $user_obj->chat_id
 		),
 		'message' => array(
-		  'text' => (strlen($message_body) > 0) ? $message_body : "__(TXT-AINT-HERE)__"
+		  'text' => $body_txt
     )
 	);
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v2.6/me/messages?access_token=EAAXFDiMELKsBAM0ukSiFZBhCHFWJIqMHhv1uwuL0GZB59PZC7AljrESQetUJRlusUTkzyMnM67Ahn9etkboS4ZCXIRoipiIUIYUh11nx3FQqDRKLxdGZCWSsONZBwQEpjV67GV7majCwB5iTUaaDPoQZC3FAIAxZCeQ5cdqhE9DSMBKS9Gzpv9Yt");
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload_arr));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-
-	$res = json_decode(curl_exec($ch), true);
-	curl_close($ch);
+	post_message($config_arr['FB_GRAPH_API'] ."?access_token=". $config_arr['FB_ACCESS_TOKEN'], $payload_arr);
+  send_tracker($user_obj->chat_id);
 }
 
 mysqli_free_result($result);
+
+// close db
+if ($db_conn) {
+	mysqli_close($db_conn);
+	$db_conn = null;
+}
 
 ?>
--- cron/php/kik-text-broadcast.php
+++ cron/php/kik-text-broadcast.php
@@ -5,6 +5,11 @@ define('DB_NAME', "db4086_modd");
 define('DB_USER', "db4086_modd_usr");
 define('DB_PASS', "f4zeHUga.age");
 
+// bot info
+define('BROADCASTER_ADDR', "162.243.150.21");
+define('BROADCASTER_USER_AGENT', "GameBots-Broadcaster-v3");
+define('KIKBOT_MESSAGE_TEMPLATE_PATH', "/opt/cron/etc/kik-text-broadcast.conf");
+
 // make the connection
 $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Could not connect to database\n");
 
@@ -12,47 +17,82 @@ $db_conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Could not connect t
 mysqli_select_db($db_conn, DB_NAME) or die("Could not select database\n");
 mysqli_set_charset($db_conn, 'utf8');
 
-$message = "";
-$handle = @fopen("/opt/modd/etc/kik-text-broadcast.conf", 'r');
+// open txt config for templates
+$config_arr = array(
+  'KIKBOT_HOST'            => "http://159.203.250.4:8080",
+  'KIKBOT_WEBHOOK'         => "/message",
+  'KIKBOT_BROADCAST_TOKEN' => "f7d3612391b5ba4d89d861bea6283726",
+  'BODY_TEMPLATE'          => "Want a chance to win a Frontside Misty? Get Gamebots on m.me/gamebotsc"
+);
+
+
+// open txt 1-liner for template
+$handle = @fopen(KIKBOT_MESSAGE_TEMPLATE_PATH, 'r');
 if ($handle) {
+  
+  // read in the line
   while (($buffer = fgets($handle, 4096)) !== false) {
-    $message .= $buffer;
+    if (preg_match('/^[#$]/i', $buffer) || preg_match('/^\s*$/i', $buffer)) {
+      continue;
+    
+    } else {
+      preg_match('/^(?P<key>[A-Z_]+)\t(?P<val>.*)$/', $buffer, $match_arr);
+      $config_arr[$match_arr['key']] = $match_arr['val'];
+    }
   }
 
+  // eof error
   if (!feof($handle)) {
-    $message = "Did you know you can win Frontside Misty's in Gamebots Facebook chatbot?\n\nm.me/gamebotsc\nFacebook.com/gamebotsc";
+    //$message_template = FB_ORTHODOX_MESSAGE_TEMPLATE;
   }
-
+  
   fclose($handle);
+  
+} else {
+  echo("Couldn't open config file! ". KIKBOT_MESSAGE_TEMPLATE_PATH ."\n");
 }
 
-if (strlen($message) == 0) {
-  $message = "Did you know you can win Frontside Misty's in Gamebots Facebook chatbot?\n\nm.me/gamebotsc\nFacebook.com/gamebotsc";
-}
+$body_txt = $config_arr['BODY_TEMPLATE'];
 
-// select gamebots users
-$query = 'SELECT DISTINCT(`username`), `chat_id` FROM `kikbot_logs`;';
+
+// use defined name as argv 'php kik-product-broadcast.php "btzDoh"'
+$query = (count($argv) == 2) ? 'SELECT `username`, `chat_id` FROM `kikbot_logs` WHERE `username` = "'. $argv[1] .'" ORDER BY `added` DESC LIMIT 1;' : 'SELECT DISTINCT(`username`), `chat_id` FROM `kikbot_logs` WHERE `active` = 1 ORDER BY `added` DESC;';
 $result = mysqli_query($db_conn, $query);
 
-while ($user_obj = mysqli_fetch_object($result)) {
-  echo ("KIK {txt}--> ". $user_obj->username ."\n");
+echo("Sending to ". number_format(mysqli_num_rows($result)) ." total users w/ message: \"". $body_txt ."\"\n[=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]\n");
 
+// iterate
+$cnt = 0;
+while ($user_obj = mysqli_fetch_object($result)) {
+
+  // output status
+  echo(sprintf("KIK ([%05s]/[%05s])--> [%-64s] \"%s\"\n", number_format(++$cnt), number_format(mysqli_num_rows($result)), $user_obj->chat_id, $user_obj->username));
+
+  // curl off to webhook
   $curl = curl_init();
   curl_setopt_array($curl, array(
-    CURLOPT_USERAGENT => "GameBots-Tracker-v2",
+    CURLOPT_USERAGENT => BROADCASTER_USER_AGENT,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => http_build_query(array(
-      'token' => "f7d3612391b5ba4d89d861bea6283726",
+      'token' => $config_arr['KIKBOT_BROADCAST_TOKEN'],
       'to_user' => $user_obj->username,
       'chat_id' => $user_obj->chat_id,
-      'message' => $message
+      'message' => $body_txt
     )),
-    CURLOPT_URL => "http://159.203.250.4:8080/message"
+    CURLOPT_URL => $config_arr['KIKBOT_HOST'] . $config_arr['KIKBOT_WEBHOOK']
   ));
 
   $r = curl_exec($curl);
   curl_close($curl);
+}
+
+mysqli_free_result($result);
+
+// close db
+if ($db_conn) {
+	mysqli_close($db_conn);
+	$db_conn = null;
 }
 
 ?>
